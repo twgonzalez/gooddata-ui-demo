03eca44f6b38fd692f5392a77d9b2b8fbe69083e
Jiri Zajic <jiri.zajic@mensa.cz>
2021-08-31 15:27:40 -0700

    prettier

--- tutorial-4/my-dashboard-app/src/components/dashboard/DashboardAreaChart.js
+++ tutorial-4/my-dashboard-app/src/components/dashboard/DashboardAreaChart.js
@@ -1,131 +1,104 @@
-import React, { useEffect, useState } from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
-import { numberFormat } from "@gooddata/numberjs";
-import { MeasureGroupIdentifier, newTwoDimensional } from "@gooddata/sdk-model";
-import { DataViewFacade } from "@gooddata/sdk-ui";
-import { workspace } from "../../constants";
-import { useBackend } from "../../contexts/Auth";
+import React, { useEffect, useState } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { numberFormat } from '@gooddata/numberjs';
+import { MeasureGroupIdentifier, newTwoDimensional } from '@gooddata/sdk-model';
+import { DataViewFacade } from '@gooddata/sdk-ui';
+import { workspace } from '../../constants';
+import { useBackend } from '../../contexts/Auth';
 
-const DashboardAreaChart = ({
-  measure,
-  viewBy,
-  stackBy,
-  filters,
-  handleAreaClick = (d, i) => {},
-}) => {
-  const backend = useBackend();
+const DashboardAreaChart = ({ measure, viewBy, stackBy, filters, handleAreaClick = (d, i) => {} }) => {
+    const backend = useBackend();
 
-  const [chartData, setChartData] = useState([]);
-  const [measureFormat, setMeasureFormat] = useState("#,###");
-  const [series, setSeries] = useState([]);
-  const [hoverDatum, setHoverDatum] = useState(null);
-  const colors = [
-    "#161E5E",
-    "#223B89",
-    "#316BA7",
-    "#BEE0CC",
-    "#419DC5",
-    "#70C3D0",
-  ];
+    const [chartData, setChartData] = useState([]);
+    const [measureFormat, setMeasureFormat] = useState('#,###');
+    const [series, setSeries] = useState([]);
+    const [hoverDatum, setHoverDatum] = useState(null);
+    const colors = ['#161E5E', '#223B89', '#316BA7', '#BEE0CC', '#419DC5', '#70C3D0'];
 
-  const handleMouseEnter = (d) => {
-    setHoverDatum(d);
-  };
-
-  const handleMouseLeave = () => {
-    setHoverDatum(null);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await backend
-        .workspace(workspace)
-        .execution()
-        .forItems([measure, viewBy, stackBy], filters)
-        .withDimensions(
-          ...newTwoDimensional([viewBy], [MeasureGroupIdentifier, stackBy])
-        )
-        .execute();
-
-      const allData = await result.readAll();
-      const dataView = DataViewFacade.for(allData);
-
-      const series = dataView.data().series().toArray();
-
-      const data = dataView.dataView.data;
-      const datums = dataView.dataView.headerItems[0][0].map(
-        (item) => item["attributeHeaderItem"].name
-      );
-      const categories = dataView.dataView.headerItems[1][1].map(
-        (item) => item["attributeHeaderItem"].name
-      );
-      const plots = data.map((row, i) => ({
-        xAxis: datums[i],
-        ...row.reduce((acc, value, j) => {
-          acc[categories[j]] = parseFloat(value);
-          return acc;
-        }, {}),
-      }));
-
-      setMeasureFormat(series[0].measureFormat());
-      setChartData(plots);
-      setSeries(categories);
+    const handleMouseEnter = (d) => {
+        setHoverDatum(d);
     };
-    fetchData();
-  }, [measure, viewBy, stackBy, filters, backend]);
 
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <AreaChart
-        width={500}
-        height={400}
-        data={chartData}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 50,
-          bottom: 15,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="xAxis" />
-        <YAxis
-          tickFormatter={(value) =>
-            numberFormat(value === -Infinity ? 0 : value, measureFormat)
-          }
-        />
-        <Tooltip
-          formatter={(value) =>
-            numberFormat(value === -Infinity ? 0 : value, measureFormat)
-          }
-        />
-        {series.map((d, i) => {
-          return (
-            <Area
-              type="monotone"
-              dataKey={d}
-              stackId="1"
-              stroke={colors[i % colors.length]}
-              fill={colors[i % colors.length]}
-              fillOpacity={d === hoverDatum ? 1 : 0.65}
-              key={i}
-              onClick={() => handleAreaClick(d, i)}
-              onMouseEnter={() => handleMouseEnter(d)}
-              onMouseLeave={() => handleMouseLeave()}
-            />
-          );
-        })}
-      </AreaChart>
-    </ResponsiveContainer>
-  );
+    const handleMouseLeave = () => {
+        setHoverDatum(null);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await backend
+                .workspace(workspace)
+                .execution()
+                .forItems([measure, viewBy, stackBy], filters)
+                .withDimensions(...newTwoDimensional([viewBy], [MeasureGroupIdentifier, stackBy]))
+                .execute();
+
+            const allData = await result.readAll();
+            const dataView = DataViewFacade.for(allData);
+
+            const series = dataView.data().series().toArray();
+
+            const data = dataView.dataView.data;
+            const datums = dataView.dataView.headerItems[0][0].map(
+                (item) => item['attributeHeaderItem'].name,
+            );
+            const categories = dataView.dataView.headerItems[1][1].map(
+                (item) => item['attributeHeaderItem'].name,
+            );
+            const plots = data.map((row, i) => ({
+                xAxis: datums[i],
+                ...row.reduce((acc, value, j) => {
+                    acc[categories[j]] = parseFloat(value);
+                    return acc;
+                }, {}),
+            }));
+
+            setMeasureFormat(series[0].measureFormat());
+            setChartData(plots);
+            setSeries(categories);
+        };
+        fetchData();
+    }, [measure, viewBy, stackBy, filters, backend]);
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+                width={500}
+                height={400}
+                data={chartData}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 50,
+                    bottom: 15,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="xAxis" />
+                <YAxis
+                    tickFormatter={(value) => numberFormat(value === -Infinity ? 0 : value, measureFormat)}
+                />
+                <Tooltip
+                    formatter={(value) => numberFormat(value === -Infinity ? 0 : value, measureFormat)}
+                />
+                {series.map((d, i) => {
+                    return (
+                        <Area
+                            type="monotone"
+                            dataKey={d}
+                            stackId="1"
+                            stroke={colors[i % colors.length]}
+                            fill={colors[i % colors.length]}
+                            fillOpacity={d === hoverDatum ? 1 : 0.65}
+                            key={i}
+                            onClick={() => handleAreaClick(d, i)}
+                            onMouseEnter={() => handleMouseEnter(d)}
+                            onMouseLeave={() => handleMouseLeave()}
+                        />
+                    );
+                })}
+            </AreaChart>
+        </ResponsiveContainer>
+    );
 };
 
 export default DashboardAreaChart;
--- tutorial-4/my-dashboard-app/src/components/dashboard/DashboardDrawer.js
+++ tutorial-4/my-dashboard-app/src/components/dashboard/DashboardDrawer.js
@@ -1,92 +1,92 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import RegionIcon from "@material-ui/icons/Public";
-import CategoryIcon from "@material-ui/icons/Category";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import ExploreIcon from "@material-ui/icons/Explore";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import RegionIcon from '@material-ui/icons/Public';
+import CategoryIcon from '@material-ui/icons/Category';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ExploreIcon from '@material-ui/icons/Explore';
 
-import * as Ldm from "../../ldm/full";
+import * as Ldm from '../../ldm/full';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
-  },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerContainer: {
+        overflow: 'auto',
+    },
 }));
 
 export const DashboardListItems = [
-  {
-    label: "Product Category",
-    attribute: Ldm.ProductCategory,
-    icon: <CategoryIcon />,
-  },
-  {
-    label: "Customer Region",
-    attribute: Ldm.CustomerRegion,
-    icon: <RegionIcon />,
-  },
+    {
+        label: 'Product Category',
+        attribute: Ldm.ProductCategory,
+        icon: <CategoryIcon />,
+    },
+    {
+        label: 'Customer Region',
+        attribute: Ldm.CustomerRegion,
+        icon: <RegionIcon />,
+    },
 ];
 
 const AppListItems = [
-  { label: "My Dashboard", icon: <DashboardIcon /> },
-  { label: "Explore", icon: <ExploreIcon /> },
+    { label: 'My Dashboard', icon: <DashboardIcon /> },
+    { label: 'Explore', icon: <ExploreIcon /> },
 ];
 
 const DashboardDrawer = ({ dimension, setDimension }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {DashboardListItems.map((item) => (
-              <ListItem
-                button
-                key={item.label}
-                selected={dimension === item}
-                onClick={() => setDimension(item)}
-              >
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.label} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {AppListItems.map((item, index) => (
-              <ListItem button key={item.label}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.label} />
-              </ListItem>
-            ))}
-          </List>
+    return (
+        <div>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <Toolbar />
+                <div className={classes.drawerContainer}>
+                    <List>
+                        {DashboardListItems.map((item) => (
+                            <ListItem
+                                button
+                                key={item.label}
+                                selected={dimension === item}
+                                onClick={() => setDimension(item)}
+                            >
+                                <ListItemIcon>{item.icon}</ListItemIcon>
+                                <ListItemText primary={item.label} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {AppListItems.map((item, index) => (
+                            <ListItem button key={item.label}>
+                                <ListItemIcon>{item.icon}</ListItemIcon>
+                                <ListItemText primary={item.label} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </Drawer>
         </div>
-      </Drawer>
-    </div>
-  );
+    );
 };
 
 export default DashboardDrawer;
